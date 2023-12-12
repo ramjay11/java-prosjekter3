c3cd51c1588d26dd3e22f14b2b897aac29ed936f
Ramjay Romorosa <ramjayromorosa@yahoo.com>
2023-12-12 23:35:56 +0800

    commit iniziale corso-spring-data-jpa

---
+++ corso-spring-data-jpa/src/main/java/com/ramjava/corso/spring/CorsoSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.ramjava.corso.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorsoSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorsoSpringDataJpaApplication.class, args);
+	}
+
+}
---
+++ corso-spring-data-jpa/src/test/java/com/ramjava/corso/spring/CorsoSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramjava.corso.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorsoSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
