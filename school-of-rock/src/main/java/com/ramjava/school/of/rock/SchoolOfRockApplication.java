b414f6e813ed043b576a3358fb91d5992b6e613a
Ramjay Romorosa <ramjayromorosa@yahoo.com>
2023-12-20 02:15:35 +0800

    school of rock innledende forplikte

---
+++ school-of-rock/src/main/java/com/ramjava/school/of/rock/SchoolOfRockApplication.java
@@ -0,0 +1,13 @@
+package com.ramjava.school.of.rock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolOfRockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolOfRockApplication.class, args);
+	}
+
+}
---
+++ school-of-rock/src/test/java/com/ramjava/school/of/rock/SchoolOfRockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramjava.school.of.rock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolOfRockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
