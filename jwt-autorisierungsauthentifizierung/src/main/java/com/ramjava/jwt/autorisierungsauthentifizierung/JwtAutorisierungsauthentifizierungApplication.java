8bd59436fbde29cdd9348ecbb8961c9ca808ad1f
Ramjay Romorosa <ramjayromorosa@yahoo.com>
2024-01-22 03:12:51 +0800

    Erster Commit der jwt-Autorisierungsauthentifizierung

---
+++ jwt-autorisierungsauthentifizierung/src/main/java/com/ramjava/jwt/autorisierungsauthentifizierung/JwtAutorisierungsauthentifizierungApplication.java
@@ -0,0 +1,13 @@
+package com.ramjava.jwt.autorisierungsauthentifizierung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAutorisierungsauthentifizierungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAutorisierungsauthentifizierungApplication.class, args);
+	}
+
+}
---
+++ jwt-autorisierungsauthentifizierung/src/test/java/com/ramjava/jwt/autorisierungsauthentifizierung/JwtAutorisierungsauthentifizierungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramjava.jwt.autorisierungsauthentifizierung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAutorisierungsauthentifizierungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
