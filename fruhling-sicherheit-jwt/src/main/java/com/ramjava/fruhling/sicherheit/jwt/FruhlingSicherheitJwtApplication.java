2165e1886584eb2c63330972d619b1e9d1df0f40
Ramjay Romorosa <ramjayromorosa@yahoo.com>
2024-01-23 03:44:58 +0800

    Erster Commit frühling-sicherheit-jwt

---
+++ fruhling-sicherheit-jwt/src/main/java/com/ramjava/fruhling/sicherheit/jwt/FruhlingSicherheitJwtApplication.java
@@ -0,0 +1,13 @@
+package com.ramjava.fruhling.sicherheit.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruhlingSicherheitJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruhlingSicherheitJwtApplication.class, args);
+	}
+
+}
---
+++ fruhling-sicherheit-jwt/src/test/java/com/ramjava/fruhling/sicherheit/jwt/FruhlingSicherheitJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramjava.fruhling.sicherheit.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruhlingSicherheitJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
