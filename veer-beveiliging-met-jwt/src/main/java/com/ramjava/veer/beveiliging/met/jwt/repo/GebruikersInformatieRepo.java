7055975c9e04e46bd4f972316aff3e80d33e5b34
Ramjay Romorosa <ramjayromorosa@yahoo.com>
2024-02-14 06:22:27 +0800

    Das Modul „veer-beveiliging-met-jwt“ wird gelöscht

--- veer-beveiliging-met-jwt/src/main/java/com/ramjava/veer/beveiliging/met/jwt/repo/GebruikersInformatieRepo.java
+++
@@ -1,7 +0,0 @@
-package com.ramjava.veer.beveiliging.met.jwt.repo;
-
-import com.ramjava.veer.beveiliging.met.jwt.entity.GebruikersInformatie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GebruikersInformatieRepo extends JpaRepository<GebruikersInformatie, String> {
-}
